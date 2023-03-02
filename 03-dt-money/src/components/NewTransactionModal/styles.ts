47fa6297495cadfadae27f0a10e57d0864b143a8
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2023-03-01 22:07:01 -0300

    removed box-shadow

--- 03-dt-money/src/components/NewTransactionModal/styles.ts
+++ 03-dt-money/src/components/NewTransactionModal/styles.ts
@@ -92,7 +92,10 @@ export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButt
   cursor: pointer;
   border: 0;
   color: ${props => props.theme['gray-300']};
-  box-shadow: none;
+  
+  &:focus {
+    box-shadow: none;
+  }
 
   svg {
     color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
