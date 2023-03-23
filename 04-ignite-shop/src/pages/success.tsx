a01f39b611e1b4d5a9929814e408d79dab7a2c06
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2023-03-23 18:50:23 -0300

    add global style

--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 export default function Success() {
+  const [data, setDate] = useState('Meu valor inicial');
+
+  if (true) {
+    console.log(data);
+  }
+  
   return (
     <h1>Success</h1>
   )
