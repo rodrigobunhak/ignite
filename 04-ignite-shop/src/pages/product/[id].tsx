40b65eadd3525e873ddb52963c0a589592c6c233
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2023-03-25 17:05:11 -0300

    add product page structure

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter();
   
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, suscipit. Culpa aspernatur rerum doloribus ipsam minus, aut sed similique molestiae. Ratione et, tempore ipsum soluta doloribus nulla magnam dolorum modi?</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
