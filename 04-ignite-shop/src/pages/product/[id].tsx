90b7ab9f99ef9ca378fc8cdfc2539af10ebd670a
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2023-03-26 09:57:56 -0300

    implemented ssg fallback

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -2,6 +2,7 @@ import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Stripe from "stripe";
 
 interface ProductProps {
@@ -15,6 +16,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+  
+  if (isFallback) {
+    return <p>Loading....</p>
+  }
+  
   return (
     <ProductContainer>
       <ImageContainer>
@@ -38,7 +45,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [
       { params: { id: 'prod_Na9gESV7oRfgtd' } }
     ],
-    fallback: false,
+    fallback: true,
   }
 }
 
