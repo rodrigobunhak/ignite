976f4726fc06fdc7ee1288fbb2867c8df4c44a60
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2023-03-27 08:02:47 -0300

    added success page structure

--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -1,3 +1,5 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
 import { useState } from "react"
 
 export default function Success() {
@@ -8,6 +10,20 @@ export default function Success() {
   }
   
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efeatuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Rodrigo</strong>, sua <strong>Camiseta XPTO</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálago
+      </Link>
+    </SuccessContainer>
   )
 }
